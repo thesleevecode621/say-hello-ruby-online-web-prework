8004db275db7ae9b24f138b5c8d1535b91b6acec
boruch d <dubywider@gmail.com>
2019-03-12 16:19:10 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,5 @@
-def say_hello(name)
-puts "Hello Kent Beck"
+def say_hello(name="Ruby Programmer")
+  puts "Hello " + name + "!"
 end
-
- def say_hello(name="Ruby Programmer!")
-  puts ("Hello #{name}!")
-end 
+say_hello("Kent Beck")
+     
